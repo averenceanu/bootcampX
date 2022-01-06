5ef2ca8736b30ebe81e609eef5185d0903c1ec46
Alexandra Verenceanu <alexandra.verenceanu@mail.mcgill.ca>
2022-01-06 04:35:33 +0000

    SQL Injection Changes

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,15 +7,18 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
+const cohortsName = process.argv[2] || 'JUL02';
+const values = [`%${cohortsName}`];
+
 pool.query(`
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers 
 JOIN assistance_requests ON teachers.id = teacher_id 
 JOIN students ON students.id = student_id 
 JOIN cohorts ON cohorts.id = cohort_id 
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+WHERE cohorts.name LIKE $1
 ORDER BY teachers.name;
-`)
+`, values)
 .then(res => {
   res.rows.forEach(row => {
     console.log(`${row.cohort}: ${row.teacher}`);
